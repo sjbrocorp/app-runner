8aa997decfc48dc1e60a43edfd44c706280cbd3b
Sam Broughton <samb@DEVE-iMac.local>
2018-07-18 17:30:17 +0100

    fine-tuning

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ export RUN_NAME=app-runner
 export WEB_NAME=vuejs-frontend
 export API_NAME=laravel-backend
 export E2E_NAME=codeceptjs-e2e-tests
-export SERVER_NAME=server
+export SERVER_NAME=nginx-server
 export PATH_TO_WEB="../web"
 export PATH_TO_API="../api"
 export PATH_TO_E2E="../e2e"
@@ -33,15 +33,21 @@ export SERVER_PORT=${SERVER_PORT:-82}
 export WEB_PORT=${WEB_PORT:-3002}
 export API_PORT=${API_PORT:-8082}
 export DB_PORT=${DB_PORT:-3307}
+export APP_ENV=${APP_ENV:-development}
 COMPOSE="docker-compose -f ./docker-compose.yml -p $APP_NAME-$SERVICE_NAME"
 
 fi
 
 if [ $# -gt 0 ]; then
-  if [ "$1" == "web" ]; then
+  if [ "$1" == "setup" ]; then
+    ./run.sh api configure
+    ./run.sh api migrate
+    ./run.sh api seed
+  elif [ "$1" == "web" ]; then
     shift 1
     if [ "$1" == "test" ]; then
-      $COMPOSE run --rm web yarn test
+      shift 1
+      $COMPOSE run --rm web yarn test "$@"
 
     elif [ "$1" == "lint" ]; then
       $COMPOSE run --rm web yarn lint
@@ -58,18 +64,15 @@ if [ $# -gt 0 ]; then
     elif [ "$1" == "test" ]; then
       shift 1
       $COMPOSE run --rm api vendor/bin/phpunit
-    elif [ "$1" == "setup" ]; then
-      ./run.sh api configure
-      ./run.sh api migrate
-      ./run.sh api seed
     elif [ "$1" == "configure" ]; then
-      cp ./api/.env.example ./api/.env
+      cp $PATH_TO_API/.env.example $PATH_TO_API/.env
+      ./run.sh api art vendor:publish --provider="Tymon\JWTAuth\Providers\LaravelServiceProvider"
       ./run.sh api art key:generate
       ./run.sh api art jwt:secret
     elif [ "$1" == "migrate" ]; then
-      $COMPOSE run --rm api php artisan migrate --seed
+      $COMPOSE run --rm api php artisan migrate
     elif [ "$1" == "seed" ]; then
-      $COMPOSE run --rm api php artisan db:seed --class=ThreadSeeder
+      $COMPOSE run --rm api php artisan db:seed
     elif [ "$1" == "dump" ]; then
       $COMPOSE run --rm api composer dump-autoload
     elif [ "$1" == "shell" ]; then
